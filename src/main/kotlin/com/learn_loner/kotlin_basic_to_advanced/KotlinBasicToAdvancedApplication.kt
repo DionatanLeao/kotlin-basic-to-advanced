0f35c9828cb1fd9cb7b645965a4931d46afb6a4d
DionatanLeao <dionatan.ti@outlook.com>
2025-01-12 05:48:43 -0300

    Project created

---
+++ src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/KotlinBasicToAdvancedApplication.kt
@@ -0,0 +1,11 @@
+package com.learn_loner.kotlin_basic_to_advanced
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBasicToAdvancedApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBasicToAdvancedApplication>(*args)
+}
