827315394d01176f0d46d63ab9665c2a34196e23
DionatanLeao <dionatan.ti@outlook.com>
2025-03-10 21:04:20 -0300

    feat: Chapter 8: Vararg Parameters in Functions

---
+++ src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/vararg/VarargParameters.kt
@@ -0,0 +1,16 @@
+package com.learn_loner.kotlin_basic_to_advanced.vararg
+
+fun main() {
+    printNumbers(0, 1)
+    printNumbers(10, 20, 30, 500)
+    val numbers = intArrayOf(1, 2, 3)
+    printNumbers(*numbers)
+    printNumbers(10, 30, *numbers, 30, 40)
+}
+
+fun printNumbers(vararg numbers: Int) {
+    for (number in numbers) {
+        print("$number ")
+    }
+    println()
+}
