bc63d57c883399d7442f7d6f4e5c61f35608a96d
DionatanLeao <dionatan.ti@outlook.com>
2025-05-09 21:38:31 -0300

    feat: Chapter 21: Coroutines

---
+++ src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_21_coroutines/Coroutines.kt
@@ -0,0 +1,9 @@
+package com.learn_loner.kotlin_basic_to_advanced.chapter_21_coroutines
+
+/*fun main(args: Array<String>) {
+    launch(CommonPool) {
+        delay(1000L)
+    }
+    println("Hello, ")
+    Thread.sleep(2000L)
+}*/
