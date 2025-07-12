43db6e4642ddcce005479a4265ae19b26ba3e210
DionatanLeao <dionatan.ti@outlook.com>
2025-07-12 20:12:20 -0300

    feat: Chapter 27: Section 27.2: Top-Level Extensions

--- src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_27_extension_methods/ExtensionMethods.kt
+++ src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_27_extension_methods/ExtensionMethods.kt
@@ -2,6 +2,8 @@ package com.learn_loner.kotlin_basic_to_advanced.chapter_27_extension_methods
 
 fun main() {
     callMyExtension(Sub())
+    val myArray = intArrayOf(1,2,3)
+    intArrayOf(4,5,6).addTo(myArray)
 }
 
 open class Super
@@ -14,4 +16,10 @@ fun Sub.myExtension() = "Defined for Sub"
 
 fun callMyExtension(myVar: Super) {
     println(myVar.myExtension())
+}
+
+fun IntArray.addTo(dest: IntArray) {
+    for (i in 0 until size) {
+        dest[1] += this[i]
+    }
 }
