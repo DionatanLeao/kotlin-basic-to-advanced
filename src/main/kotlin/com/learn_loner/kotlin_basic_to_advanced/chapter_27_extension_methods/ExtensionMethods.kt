d63df482336cd2fa3a2139593a1ff1f7846ff560
DionatanLeao <dionatan.ti@outlook.com>
2025-07-16 21:27:16 -0300

    feat: Chapter 27: Extension functions to Companion Objects (appearance of Static functions)

--- src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_27_extension_methods/ExtensionMethods.kt
+++ src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_27_extension_methods/ExtensionMethods.kt
@@ -1,6 +1,5 @@
 package com.learn_loner.kotlin_basic_to_advanced.chapter_27_extension_methods
 
-import java.lang.Math.pow
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -13,8 +12,8 @@ import kotlin.math.pow
 
 fun main() {
     callMyExtension(Sub())
-    val myArray = intArrayOf(1,2,3)
-    intArrayOf(4,5,6).addTo(myArray)
+    val myArray = intArrayOf(1, 2, 3)
+    intArrayOf(4, 5, 6).addTo(myArray)
 //    val colorCache = mutableMapOf<KColor, Color>()
 //    val KColor.color: Color
 //        get() = colorCache.getOrPut(this) {
@@ -29,6 +28,8 @@ fun main() {
     val x: Path = Paths.get("dirName").apply {
         if (Files.notExists(this)) throw IllegalStateException("The important file does not exist")
     }
+    Something.fromString("")
+//    SomethingElse.fromString("")
 }
 
 open class Super
@@ -50,18 +51,32 @@ fun IntArray.addTo(dest: IntArray) {
 }
 
 class KColor(val value: Int)
+
 fun Path.exists(): Boolean = Files.exists(this)
 fun Path.notExists(): Boolean = !this.exists()
 fun Path.deleteRecursively(): Boolean = this.toFile().deleteRecursively()
 fun Long.humanReadable(): String {
     if (this <= 0) return "0"
-    val units = arrayOf("B","KB","MB","GB","TB","EB")
-    val digitGroups = (log10(this.toDouble()) /log10(1024.0)).toInt()
+    val units = arrayOf("B", "KB", "MB", "GB", "TB", "EB")
+    val digitGroups = (log10(this.toDouble()) / log10(1024.0)).toInt()
     return DecimalFormat("#,##0.#")
-        .format(this/ 1024.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
+        .format(this / 1024.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
 }
+
 fun Int.humanReadable(): String {
     return this.toLong().humanReadable()
 }
 
 fun Temporal.toIsoString(): String = DateTimeFormatter.ISO_INSTANT.format(this)
+
+class Something {
+    companion object {}
+}
+
+class SomethingElse {
+    companion object {}
+}
+
+fun Something.Companion.fromString(s: String): Something = Something()
+
+fun SomethingElse.fromString(s: String): SomethingElse = SomethingElse()
