1adbf3f99d75bc0d98ae04b419aba1546166ce63
DionatanLeao <dionatan.ti@outlook.com>
2025-07-21 18:40:01 -0300

    feat: Chapter 29: Idioms

--- src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_29_idioms/Idioms.kt
+++ src/main/kotlin/com/learn_loner/kotlin_basic_to_advanced/chapter_29_idioms/Idioms.kt
@@ -3,7 +3,15 @@ package com.learn_loner.kotlin_basic_to_advanced.chapter_29_idioms
 import java.io.Serializable
 
 fun main() {
+    val str = "foo"
+    str.let {
+        println(it)
+    }
 
+    val str1: String = someFun()
+    str1?.let {
+        println(it)
+    }
 }
 
 class MySpecialCase : Serializable {
@@ -24,3 +32,11 @@ class MySpecialCase3 : Serializable {
         private val serialVersionUID: Long = 123
     }
 }
+
+fun someFun() = "foo"
+
+//fun someOtherAction() = ""
+//fun doSomething() {
+//    someOtherAction()
+//    return this
+//}
